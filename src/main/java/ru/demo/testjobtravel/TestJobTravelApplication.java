a1eaeac91235da15e8f02cacb051be9f72574005
Wan-Derer <frd-ru@yandex.ru>
2025-03-30 12:10:00 +0300

    Initial commit

---
+++ src/main/java/ru/demo/testjobtravel/TestJobTravelApplication.java
@@ -0,0 +1,13 @@
+package ru.demo.testjobtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJobTravelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestJobTravelApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/demo/testjobtravel/TestJobTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.demo.testjobtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJobTravelApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
